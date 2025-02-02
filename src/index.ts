baa80a7ba1f93d51db898eb5e141e02911e3b566
VitorHugost <vitorhugo.profile.dev@gmail.com>
2025-02-02 04:58:22 -0400

    last teste

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { app } from "./configs/express";
 // import dotenv from "dotenv";
 // dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`);
