b972f4435a83d10d4086c5fe2a76ee68c5bb7bb6
VitorHugost <vitorhugo.profile.dev@gmail.com>
2025-02-02 03:49:59 -0400

    mudo env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { app } from "./configs/express";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const port = process.env.PORT;
 
-// app.listen(port, () => {
-//   console.log(`Server listen on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
+});
 
-export default app;
+// export default app;;
