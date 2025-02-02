73d7410281db5211f7efcb7f8f8474e6307e00ea
VitorHugost <vitorhugo.profile.dev@gmail.com>
2025-02-02 14:02:03 -0400

    corrige vercel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { app } from "./configs/express";
+import { routes } from "./routes/index";
 // import dotenv from "dotenv";
 // dotenv.config();
-
+app.use(routes);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
